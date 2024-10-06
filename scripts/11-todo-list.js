4b23f0dcd767944fbe3b7fbc3ed4fbfaa6046795
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-06 08:52:28 +0300

    todo list is done

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,6 +1,23 @@
 // initialize an empty array 
 const todoList = [];
 
+renderToDoList();
+
+function renderToDoList(){
+    let todoListHTML = '';
+
+    for(let i = 0;i<todoList.length;i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML+=html;
+    }
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
 
@@ -10,4 +27,6 @@ function addTodo() {
     console.log(todoList);
 
     inputElement.value = '';
+
+    renderToDoList();
 }
