c3546b3e9226c6e50b31dadf88eca6da1ac60dd1
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-08 17:22:10 +0300

    todo list project done

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,8 @@
 // initialize an empty array 
+
+// splice() takes two valeus the first one is the index of element we want to remove
+// the second value is determines how many values we want to remove
+
 const todoList = [];
 
 renderToDoList();
@@ -7,8 +11,22 @@ function renderToDoList(){
     let todoListHTML = '';
 
     for(let i = 0;i<todoList.length;i++){
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        // it is same as the codes above
+        const {name,dueDate} = todoObject;
+        const html =`
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick = "
+                todoList.splice(${i},1);
+                renderToDoList();
+            " class = "delete-todo-button" >Delete</button>
+
+
+            
+        `;
         todoListHTML+=html;
     }
 
@@ -20,13 +38,23 @@ function renderToDoList(){
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
+    const dateInputElement = document.querySelector('.js-due-date-input');
 
     const name = inputElement.value;
+    const dueDate = dateInputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
+    if(name === ''){
+        alert('You should enter a todo name');
+    }else {
+        //todoList.push({name : name , dueDate : dueDate});
+        // it does the same thing the code above  
+        todoList.push({name , dueDate});
+        console.log(todoList);
 
-    inputElement.value = '';
+        console.log(name);
 
-    renderToDoList();
+        inputElement.value = '';
+
+        renderToDoList();
+    }
 }
